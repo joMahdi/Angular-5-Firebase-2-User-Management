d89423092713fe9565ba0380528ef88eeb551579
mahdi joulali <35194567+joMahdi@users.noreply.github.com>
2018-02-05 04:42:24 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,9 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-       apiKey: "AIzaSyBaaegvQxYMUmeBRdkq6Cmp4vYnL6X4udE",
-       authDomain: "clientpanel-fdc7c.firebaseapp.com",
-       databaseURL: "https://clientpanel-fdc7c.firebaseio.com",
-       storageBucket: "clientpanel-fdc7c.appspot.com",
-       messagingSenderId: "252586174365"
+       apiKey: '<your-key>',
+       authDomain: '<your-project-authdomain>',
+       databaseURL: '<your-database-URL>',
+       storageBucket: '<your-storage-bucket>'
   }
 };
